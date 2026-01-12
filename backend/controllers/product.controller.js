2395ec303a2d8190fb633c88a1dd3cf5e4abb951
monika-oss <monikababu18@gmail.com>
2026-01-12 20:42:28 +0530

    frontend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -41,35 +41,59 @@ export const postProduct=async(req,res)=>{
   }
 }
 
-export const updateProduct=async(req,res)=>{
-   const product = req.body; //user will send this data
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;        // ✅ get id from URL
+  const product = req.body;
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ sucess: false, message: "Please provide all fields" });
+  // validate MongoDB id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid product id",
+    });
   }
-  const newProduct = await Product(product);
+
+  // // field validation
+  // if (!product.name || !product.price || !product.image) {
+  //   return res.status(400).json({
+  //     success: false,
+  //     message: "Please provide all fields",
+  //   });
+  // }
 
   try {
-    await newProduct.save();
-    return res.status(201).json({ success: true, data: newProduct });
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      product,
+      { new: true } // return updated document
+    );
+
+    return res.status(200).json({
+      success: true,
+      data: updatedProduct,
+    });
   } catch (error) {
-    console.log("error in create product:", error.message);
-    res.status(400).json({ success: false, message: "server error" });
+    console.log("error in update product:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
   }
-}
+};
+
 
 export const deleteOneProduct=async(req,res)=>{
    const { id } = req.params;
-
+     if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({success:false,message:"invalid product id"});
+  }
   try {
     await Product.findByIdAndDelete(id);
     return res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
     console.log("error in fetching and delete the products:", error.message);
     return res
-      .status(404)
-      .json({ success: false, message: "Product not found" });
+      .status(500)
+      .json({ success: false, message: "server error" });
   }
 }
---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>
+)
